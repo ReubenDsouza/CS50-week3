7e2c333c214cbc542fe01f2ca0d0b6e745c07f28
ReubenDsouza <31122327+ReubenDsouza@users.noreply.github.com>
2017-12-31 15:46:21 +0530

    my find(less comfy )

---
+++ helpers.c
@@ -0,0 +1,68 @@
+/**
+ * helpers.c
+ *
+ * Helper functions for Problem Set 3.
+ */
+ 
+#include <cs50.h>
+
+#include "helpers.h"
+
+/**
+ * Returns true if value is in array of n values, else false.
+ */
+bool search(int value, int values[], int n)
+{
+    // TODO: implement a searching algorithm
+    if (n<=0)
+    {
+        return false;    
+    }
+    int a=0;
+    while(n>a)
+    {
+        int mid = (a+n-1)/2;
+        
+        if (values[mid]>value)
+        {
+            return a=mid+1;
+        }
+        else if (values[mid]<value)
+        {
+            return n=mid-1;
+        }
+        else if (values[mid]==value)
+        {
+            return true;
+        }
+    } 
+    return false;
+}
+
+/**
+ * Sorts array of n values.
+ */
+void sort(int values[], int n)
+{
+    // TODO: implement a sorting algorithm
+    int swap_counter=-1;
+    while(swap_counter!=0)
+    {
+        swap_counter=0;
+        for (int i=0;i<n-2;i++)
+        {
+            
+            if(values[i]>values[i+1])
+            {
+                int x=values[i];
+                values[i]=values[i+1];
+                values[i+1]=x;
+                swap_counter++;
+            }
+            
+        }
+        
+    }
+    
+    return;
+}
